0dad4800a817aaf9008efe21b25d93c5aaea6631
nadeesamaraweera <nadeesamaraweera2000@gmail.com>
2024-08-04 09:40:47 +0530

    first commit

---
+++ src/main/java/lk/ijse/gdse/aad/AppInit.java
@@ -0,0 +1,11 @@
+package lk.ijse.gdse.aad;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class AppInit {
+    public static void main(String[] args){
+        var ctx =new AnnotationConfigApplicationContext();
+
+
+    }
+}
